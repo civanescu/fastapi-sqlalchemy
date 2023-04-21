00e9f708afc0c10b5d11824f4d326567b856b260
Claudiu Ivanescu <civanescu@gmail.com>
2023-04-21 10:17:59 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,16 @@ import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
+import os
+import sys
+
+# Add parent directory to the sys.path so that we can import main
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from main import CarItem, app
+
+
+
 from main import CarItem, app
 
 
